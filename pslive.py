131e96c77721d76c087ab67c8f6264c80a4b1445
tom.cai <tom.cai@coupang.com>
2017-11-04 14:51:56 -0700

    changed to use json instead of yaml due to json being built-in to standard lib while yaml is not in python3

--- pslive.py
+++ pslive.py
@@ -5,32 +5,32 @@
 ## author: Tom Cai <1tomcai@gmail.com>
 ## date  : 2017-10-31
 ## pseudo code
-* two data files (data.yaml, wip.yaml) are stored in <HOME DIR>/.pslive folder. pslive.py will create this folder if not exist
-* if data.yaml does not exist or too old (default age 3600 seconds)
-  - fetch live data from internet and save the data to data.yaml (more work to be done here, or you can add your own code to fetch live data)
-* if wip.yaml does not exist
-  - get a random entry in data.yaml
+* two data files (data.json, wip.json) are stored in <HOME DIR>/.pslive folder. pslive.py will create this folder if not exist
+* if data.json does not exist or too old (default age 3600 seconds)
+  - fetch live data from internet and save the data to data.json (more work to be done here, or you can add your own code to fetch live data)
+* if wip.json does not exist
+  - get a random entry in data.json
   - display the key
-  - save the key to wip.yaml
-* if wip.yaml exists
-  - read wip.yaml and compare to the corresponding entry in data.yaml
-  - if they equel, then repeat steps as if wip.yaml file does not exist
-  - if they do not equel, display the next value that is not in the wip.yaml and update wip.yaml to add the displayed value list
+  - save the key to wip.json
+* if wip.json exists
+  - read wip.json and compare to the corresponding entry in data.json
+  - if they equel, then repeat steps as if wip.json file does not exist
+  - if they do not equel, display the next value that is not in the wip.json and update wip.json to add the displayed value list
 
 ## Use cases
-1. flashcard: data.yaml stores words and their definition, example sentences, descriptions, etc.
-2. news flash: data.yaml stores headlines and short stores
-3. reminder: data.yaml stores tasks / due dates
+1. flashcard: data.json stores words and their definition, example sentences, descriptions, etc.
+2. news flash: data.json stores headlines and short stores
+3. reminder: data.json stores tasks / due dates
 """
 
-import yaml
+import json
 import random
 import os
 import time
 
 PSLIVE_DIR = '%s/.pslive' % os.getenv("HOME")
-CURRENT_FILE = 'wip.yaml'
-DATA_FILE = 'data.yaml'
+CURRENT_FILE = 'wip.json'
+DATA_FILE = 'data.json'
 MAX_AGE = 900 # seconds = 15 minutes
 
 def get_entries(infile, key=None):
@@ -42,7 +42,7 @@ def get_entries(infile, key=None):
     '''
     try:
         with open(infile) as f:
-            entries = yaml.load(f.read())
+            entries = json.load(f.read())
 
         return {key: entries[key]} if key else entries
 
@@ -54,7 +54,7 @@ def get_live_data(source, kind):
     '''
     source: url
     kind: tbd
-    output: None. side effort is the ~/.pslive/data.yaml will be populated
+    output: None. side effort is the ~/.pslive/data.json will be populated
     notes: currently simulate live data feed by copying a local file
     '''
     import shutil
@@ -74,13 +74,14 @@ def file_obsolete(fname, max_age):
 def show_random_key(wip_file, data_entries):
     '''
     wip_file: file name containing the current working entry
-    data_entries: dictionary containing full entries from ~/.pslive/data.yaml
+    data_entries: dictionary containing full entries from ~/.pslive/data.json
     output: None. Side effort is to display a random key from data_entries and populate 'Key:' to the wip_file
     '''
     try:
         key = random.choice(list(data_entries.keys()))
+        key_null = {key: None}
         with open(wip_file, 'w') as f:
-            f.write('{}:'.format(key))
+            json.dump(key_null, f)
         print(key)
     except Exception as e:
         print('error:', e)
@@ -89,7 +90,7 @@ def show_random_key(wip_file, data_entries):
 def display_next(wip_file, partial_entry, data_entries):
     '''
     partial_entry: a dictionary for the current entry, may contain none or partial values
-    data_entries: a dictionary for the whole data entries from ~/.pslive/data.yaml
+    data_entries: a dictionary for the whole data entries from ~/.pslive/data.json
     output: None. Side effort is to display the next value for the current key
     '''
     key = next(iter(partial_entry))
@@ -110,7 +111,7 @@ def display_next(wip_file, partial_entry, data_entries):
 
             print(next_val)
             with open(wip_file, 'w') as f:
-                yaml.dump(partial_entry, f)
+                json.dump(partial_entry, f)
             break
 
 def main():
@@ -123,7 +124,7 @@ def main():
             get_live_data('~/words.txt', 'localfile')
 
         with open(data_file) as f:
-            data_entries = yaml.load(f)
+            data_entries = json.load(f)
 
     except Exception as e:
         print(e)
@@ -133,7 +134,7 @@ def main():
         wip_file = "%s/%s" % (PSLIVE_DIR, CURRENT_FILE)
 
         with open(wip_file) as f:
-            partial_entry = yaml.load(f)
+            partial_entry = json.load(f)
 
         display_next(wip_file, partial_entry, data_entries)
 
